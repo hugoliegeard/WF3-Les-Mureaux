89c3b2711841b31c16467821d0466dc4183b8178
Hugo LIEGEARD <hugo@hl-media.co>
2018-04-19 14:33:12 +0200

    Travail du 19/04/18
    Finalisation de Javascript.
    - Correction Exo 4
    - Chapitre sur la Gestion du Temps.

---
+++ 01-JS/assets/js/17.js
@@ -0,0 +1,26 @@
+/* -------------------------------
+      LA GESTION DU TEMPS ⌚️💣
+-------------------------------- */
+
+/**
+ * La fonction " setTimeout " me permet de 
+ * spécifier une fonction à executer au 
+ * bout d'une durée définie.
+ */
+
+ function Hello() {
+     alert('Bonjour toi ! Que fais-tu ici ?');
+ }
+
+ /**
+  * 1. La fonction à executer
+  * 2. La durée en millisecondes.
+  */
+ setTimeout(Hello, 3000);
+
+ /**
+  * La fonction " setInterval " me permet d'executer
+  * une fonction à un intervalle régulier défini.
+  */
+ 
+  setInterval(Hello, 3000);
